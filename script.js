6f0dcbb29e025f6736279b910cf863ac0ad98c1f
DeathCat73 <eddydrowett@gmail.com>
2021-08-23 18:18:08 +0100

    fixforjsfixfixplsfingworkaaa

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function start() {
-  var max = document.getElementById(max).innerText.toString()
+  var max = document.getElementById(max)[0].value.innerText.toString()
   document.getElementById("result").innerText = (Math.floor(Math.random * parseInt(max)))
 }
 
