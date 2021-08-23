3267c0cc2ab41dde9f16e71282813889d827ae94
DeathCat73 <eddydrowett@gmail.com>
2021-08-23 18:12:11 +0100

    jsfixfixplsplswork

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function start() {
-  var max = document.getElementById(max).innerText.toString;
-  document.getElementById("result").innerText = (Math.floor(Math.random * parseInt(max)));
+  var max = document.getElementById(max).innerText.toString()
+  document.getElementById("result").innerText = (Math.floor(Math.random * parseInt(max)))
 }
 
   
